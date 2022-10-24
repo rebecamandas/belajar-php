10287ebb2708936b431e63b3e38b2178e1573064
Laura Rebeca Mandas <rebecamandas.rm@gmail.com>
2022-10-24 11:59:17 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Laura Rebeca Mandas";
+$umur = 19;
+
+echo "Nama saya <strong>$nama</strong>, saya $umur tahun.<br>";
+
+$namaAdik = "Freddy";
+$umurAdik = "16";
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+?>
